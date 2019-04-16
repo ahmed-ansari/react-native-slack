7e181ce17b782a4ad143543a77b45a5555d84281
Ahmed Ansari <toahmedshah@gmail.com>
2019-04-16 09:28:24 +0530

    remove config data

--- src/firebase.js
+++ src/firebase.js
@@ -6,12 +6,7 @@ import 'firebase/database';
 import 'firebase/storage';
 
 var config = {
-    apiKey: "AIzaSyAaiCItVDfuiS8I_Nmytsf44LJwDrRxrso",
-    authDomain: "react-native-slack.firebaseapp.com",
-    databaseURL: "https://react-native-slack.firebaseio.com",
-    projectId: "react-native-slack",
-    storageBucket: "react-native-slack.appspot.com",
-    messagingSenderId: "313269807605"
+
 };
 firebase.initializeApp(config);
 
