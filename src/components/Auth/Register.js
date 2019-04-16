79a8c22db0210e4818b021593f32177c9f3f1139
Ahmed Ansari S <toahmedshah@gmail.com>
2019-04-16 23:54:54 +0530

    added  firebase auth and completed login registeration

--- src/components/Auth/Register.js
+++ src/components/Auth/Register.js
@@ -10,6 +10,7 @@ import {
   } from "semantic-ui-react";
   import { Link } from "react-router-dom";
 import firebase from '../../firebase';
+import md5 from "md5";
 
 
 export class Register extends Component {
@@ -18,29 +19,119 @@ export class Register extends Component {
         username: "",
         email: "",
         password: "",
-        passwordConfirmation: ""
+        passwordConfirmation: "",
+        errors: [],
+        loading: false,
+        usersRef: firebase.database().ref("users")
       };
 
+      isFormValid = () => {
+        let errors = [];
+        let error;
+    
+        if (this.isFormEmpty(this.state)) {
+          error = { message: "Fill in all fields" };
+          this.setState({ errors: errors.concat(error) });
+          return false;
+        } else if (!this.isPasswordValid(this.state)) {
+          error = { message: "Password is invalid" };
+          this.setState({ errors: errors.concat(error) });
+          return false;
+        } else {
+          return true;
+        }
+      };
+    
+      isFormEmpty = ({ username, email, password, passwordConfirmation }) => {
+        return (
+          !username.length ||
+          !email.length ||
+          !password.length ||
+          !passwordConfirmation.length
+        );
+      };
+    
+      isPasswordValid = ({ password, passwordConfirmation }) => {
+        if (password.length < 6 || passwordConfirmation.length < 6) {
+          return false;
+        } else if (password !== passwordConfirmation) {
+          return false;
+        } else {
+          return true;
+        }
+      };
+    
+      displayErrors = errors =>
+        errors.map((error, i) => <p key={i}>{error.message}</p>);
+      
     handleChange = event => {
     this.setState({ [event.target.name]: event.target.value });
     };
 
     handleSubmit = event => {
         event.preventDefault();
+        if (this.isFormValid()) {
+          this.setState({ errors: [], loading: true });
         firebase
           .auth()
           .createUserWithEmailAndPassword(this.state.email, this.state.password)
           .then(createdUser => {
             console.log(createdUser);
+            // this.setState({ loading: false });
+            createdUser.user
+            .updateProfile({
+              displayName: this.state.username,
+              photoURL: `http://gravatar.com/avatar/${md5(
+                createdUser.user.email
+              )}?d=identicon`
+            })
+            .then(() => {
+              this.saveUser(createdUser).then(() => {
+                console.log("user saved");
+              });
+            })
+            .catch(err => {
+              console.error(err);
+              this.setState({
+                errors: this.state.errors.concat(err),
+                loading: false
+              });
+            });
           })
           .catch(err => {
             console.error(err);
+            
+          this.setState({
+            errors: this.state.errors.concat(err),
+            loading: false
           });
+          });
+    }
       };
 
-  render() {
-    const { username, email, password, passwordConfirmation } = this.state;
+      saveUser = createdUser => {
+        return this.state.usersRef.child(createdUser.user.uid).set({
+          name: createdUser.user.displayName,
+          avatar: createdUser.user.photoURL
+        });
+      };
+    
 
+      handleInputError = (errors, inputName) => {
+        return errors.some(error => error.message.toLowerCase().includes(inputName))
+          ? "error"
+          : "";
+      };
+    
+      render() {
+        const {
+          username,
+          email,
+          password,
+          passwordConfirmation,
+          errors,
+          loading
+        } = this.state;
     return (
         <Grid textAlign="center" verticalAlign="middle" className="app">
         <Grid.Column style={{ maxWidth: 450 }}>
@@ -69,7 +160,9 @@ export class Register extends Component {
                 placeholder="Email Address"
                 onChange={this.handleChange}
                 value={email}
+                className={this.handleInputError(errors, "email")}
                 type="email"
+
               />
 
               <Form.Input
@@ -80,6 +173,8 @@ export class Register extends Component {
                 placeholder="Password"
                 onChange={this.handleChange}
                 value={password}
+                className={this.handleInputError(errors, "password")}
+
                 type="password"
               />
 
@@ -91,14 +186,27 @@ export class Register extends Component {
                 placeholder="Password Confirmation"
                 onChange={this.handleChange}
                 value={passwordConfirmation}
+                className={this.handleInputError(errors, "password")}
                 type="password"
               />
 
-              <Button color="orange" fluid size="large">
+<Button
+                disabled={loading}
+                className={loading ? "loading" : ""}
+                color="orange"
+                fluid
+                size="large"
+              >
                 Submit
               </Button>
             </Segment>
           </Form>
+          {errors.length > 0 && (
+            <Message error>
+              <h3>Error</h3>
+              {this.displayErrors(errors)}
+            </Message>
+          )}
           <Message>
             Already a user? <Link to="/login">Login</Link>
           </Message>
--- src/index.js
+++ src/index.js
@@ -1,23 +1,41 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import "semantic-ui-css/semantic.min.css";
+import firebase from "./firebase";
+import {BrowserRouter as Router, Switch, Route, withRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import {BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Login from './components/Auth/Login';
 import Register from './components/Auth/Register';
-import "semantic-ui-css/semantic.min.css";
 
 
-const Root = () =>  (
-    <Router>
+class Root extends React.Component {
+    componentDidMount() {
+      firebase.auth().onAuthStateChanged(user => {
+        console.log('user', user)
+        if (user) {
+          this.props.history.push("/");
+        }
+      });
+    }
+  
+    render() {
+      return (
         <Switch>
-            <Route  exact path="/" component={App} />
-            <Route  path="/login" component={Login} />
-            <Route  path="/register" component={Register} />
+          <Route exact path="/" component={App} />
+          <Route path="/login" component={Login} />
+          <Route path="/register" component={Register} />
         </Switch>
-    </Router>
-);
-
-ReactDOM.render(<Root />, document.getElementById('root'));
-registerServiceWorker();
+      );
+    }
+  }
+  
+  const RootWithAuth = withRouter(Root);
+  
+  ReactDOM.render(
+    <Router>
+      <RootWithAuth />
+    </Router>,
+    document.getElementById("root")
+  );registerServiceWorker();
